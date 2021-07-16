e2494e27c095945146868f0a5fdc54e539df590a
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-16 14:41:45 -0400

    Navbar component added

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled , {css} from "styled-components/macro";
+import {Link} from "react-router-dom"
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import {FaBars} from "react-icons/fa"
+
+const Nav = styled.nav `
+    height:60px;
+    display: flex;
+    justify-content:space-between;
+    padding: 1rem 2rem;
+    z-index:100;
+    positon: fixed;
+    width:100%;
+    background:red;
+    
+`
+
+const NavLink=css`
+color:#fff;
+display:flex:
+align-items:center;
+padding:0 1rem;
+height:100%;
+cursor:pointer;
+text-decoration:none;
+`;
+
+const Logo = styled(Link)`
+    ${NavLink}
+    font-style:italic;
+`
+
+const MenuBars = styled(FaBars)`
+    display:none;
+
+    @media screen and (max-width: 768px) {
+    color:white;
+    display:block;
+    background-size:contain;
+    height:40px;
+    width:40px;
+    cursor:pointer;
+    position:absolute;
+    top:0;
+     right: 0;
+     transform: translate(-50%,25%);
+    }
+`;
+
+const NavMenu = styled.div`
+display:flex;
+align-items:center;
+margin-right:-48px;
+
+@media screen and (max-width: 768px) {
+    display:none;
+}
+`;
+
+const NavMenuLinks= styled(Link)`
+${NavLink}
+`;
+
+const NavButton = styled.div`
+display:flex;
+align-items:center;
+margin-right:24px;
+
+@media screen and (max-width: 768px) {
+    display:none;
+}
+`;
+
+function Navbar() {
+    return (
+        <div>
+        <Nav>
+            <Logo to="/">HOMIX</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item,index)=>(
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+            <NavButton>
+                <Button to="/contact" primary="true">Contact us</Button>
+            </NavButton>
+        </Nav>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ my-app/src/globalStyles.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle= createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+
+html,body{
+    overflow-x: hidden;
+};
+
+`;
+
+export default GlobalStyle;
