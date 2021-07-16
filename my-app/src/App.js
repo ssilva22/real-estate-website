1442c3e0cf6b378d72b957f74142d0271127915b
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-16 18:00:57 -0400

    Info Section added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,25 @@ import Hero from "./components/Hero";
 import GlobalStyle from "./globalStyles";
 import { SliderData } from "./data/SliderData";
 import Dropdown from "./components/Dropdown";
+import InfoSection from "./components/InfoSection";
+import { InfoData } from "./data/InfoData";
 
 function App() {
   const [isOpen,setIsOpen] = useState(false);
+
+
   const toggle = () => {
     setIsOpen(!isOpen)
   }
+
+
   return (
     <>
     <GlobalStyle/>
       <Navbar toggle={toggle}/>
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={SliderData}/>
+      <InfoSection {...InfoData} />
     </>
   );
 }
---
+++ my-app/src/data/InfoData.js
@@ -0,0 +1,34 @@
+import ImageOne from "../images/house-7.jpg"
+import ImageTwo from "../images/house-8.jpg"
+
+export const InfoData = {
+    heading:"Explore our beautiful homes",
+    paragrapOne: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices, eros non interdum placerat, nibh magna consequat orci, a ornare sapien eros sed nibh. Vivamus id leo pulvinar,
+     consequat sem suscipit, porta sapien.
+     Cras in orci eget dui varius malesuada eu vitae justo.`,
+
+    paragrapTwo: `Sed cursus lectus urna, 
+    a varius elit imperdiet ac. Integer 
+    ornare urna ut mi maximus mollis. 
+    Etiam eu ante eget metus suscipit 
+    facilisis vitae sed diam. Integer quis lacus fermentum,`,
+    buttonLabel: "View Homes",
+    image: ImageOne,
+    reverse:false,
+    delay:100
+}
+
+export const InfoDataTwo = {
+    heading:"Modern Designs",
+    paragrapOne: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices, eros non interdum placerat, nibh magna consequat orci, a ornare sapien eros sed nibh. Vivamus id leo pulvinar,
+     consequat sem suscipit, porta sapien.
+     Cras in orci eget dui varius malesuada eu vitae justo.`,
+
+    paragrapTwo: `Sed cursus lectus urna, 
+    a varius elit imperdiet ac. Integer 
+    ornare`,
+    buttonLabel: "View Homes",
+    image: ImageTwo,
+    reverse:true,
+    delay:300
+}
